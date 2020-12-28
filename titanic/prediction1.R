7d45778c9e041ac3063786c54de7a78cd79f9db5
vidyut07 <vidyut07@gmail.com>
2020-12-28 02:15:30 -0500

    first and second titanic problem R scipts

---
+++ titanic/prediction1.R
@@ -0,0 +1,20 @@
+
+#set the working directory
+setwd("E:/Google Drive(biddutbhowmic.dev)/ml-workbook/titanic")
+
+# Import the training set: train
+# Your working directory might vary
+
+train <- read.csv("train.csv")
+
+# viewing the "train" dataframe in raw format
+
+train
+
+# viewing the "train" dataframe in table format
+
+View(train)
+
+# dataframe structure
+
+str(train)
